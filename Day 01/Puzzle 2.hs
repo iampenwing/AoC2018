fda4f663d07d2389ba69db90fd42261efdf7bf0b
penwing <penwing@penwing.me.uk>
2018-12-01 23:36:40 +0000

    Day 1, Puzzle 2 Complete!

---
+++ Day 01/Puzzle 2.hs
@@ -0,0 +1,41 @@
+-- AdventOfCode Day 1, Puzzle 2
+-- Chronal Calibration
+-- https://adventofcode.com/2018/day/1
+-- Adds/subtracts all numbers in an input
+
+-- Alex Lambert (penwing)
+-- aoc@penwing.me.uk
+
+-- import File
+import Data.String
+import System.Environment
+
+myReadInt :: [Char] -> Int
+myReadInt ('+':xs) = read xs :: Int
+myReadInt x        = read x :: Int
+
+isIn :: Int -> [Int] -> Bool
+isIn _ []     = False
+isIn x (y:ys)
+  | x == y    = True
+  | True      = isIn x ys
+
+runFreq :: Int -> [Int] -> [Int] -> (Bool, Int, [Int])
+runFreq s [] freqList            = (False, s, freqList)
+runFreq s (x:xs) freqList = let newFreq = s + x in
+                              if (isIn newFreq freqList)
+                                 then (True, newFreq, freqList)
+                                 else runFreq newFreq xs (newFreq:freqList)
+
+runFreqs :: String -> Int -> [Int] -> IO ()
+runFreqs fileContents n fList = let (found, value, freqList) = runFreq n (map myReadInt (lines fileContents)) fList in
+    if found
+       then putStrLn (show value)
+       else runFreqs fileContents value freqList
+
+main :: IO()
+main = do
+  [fileInput, _] <- getArgs
+  fileContents <- readFile fileInput
+  runFreqs fileContents 0 []
+    
