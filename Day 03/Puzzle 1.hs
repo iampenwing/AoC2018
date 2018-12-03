f85a4c9b4afaffb6ba377ae563c5a577c463fcd8
penwing <penwing@penwing.me.uk>
2018-12-03 22:16:54 +0000

    tidied up makeClaim1 function by using list comprehensions rather than recursion

--- Day 03/Puzzle 1.hs
+++ Day 03/Puzzle 1.hs
@@ -26,21 +26,17 @@ getNum (x:xs) y
 
 makeClaim1 :: [[Int]] -> [((Int, Int), [Int], Int)] -> [((Int, Int), [Int], Int)] 
 makeClaim1 [] claims = claims
-makeClaim1 ((id:(x:(y:(w:(h:[]))))):rest) claims = makeClaim1 rest (makeClaimX1 id x y h w claims) 
-
-makeClaimX1 :: Int -> Int -> Int -> Int -> Int -> [((Int, Int), [Int], Int)] -> [((Int, Int), [Int], Int)]
-makeClaimX1 id x y w 0 claims = claims
-makeClaimX1 id x y w h claims = makeClaimX1 id x y w (h - 1) (makeClaimY1 id ((x + h) - 1) y w claims)
-
-makeClaimY1 :: Int -> Int -> Int -> Int -> [((Int, Int), [Int], Int)]  -> [((Int, Int), [Int], Int)] 
-makeClaimY1 id x y 1 claims = insertClaim1 ((x, y), id) claims
-makeClaimY1 id x y w claims = makeClaimY1 id x y (w - 1) (insertClaim1 ((x, ((y + w) -1)), id) claims)
+makeClaim1 ((id:(x:(y:(w:(h:[]))))):rest) claims = insertClaims [(((x + nX), (y + nY)), id) | nX <- [0..(w - 1)] , nY <- [0..(h - 1)]] (makeClaim1 rest claims)
 
 insertClaim1 :: ((Int, Int), Int) -> [((Int, Int), [Int], Int)] -> [((Int, Int), [Int], Int)]
 insertClaim1 ((claimX, claimY), claimID) [] = [((claimX, claimY), [claimID], 1)]
 insertClaim1 ((claimX, claimY), claimID) (((firstX, firstY), firstIDs, firstCount):claims)
   | (claimX == firstX) && (claimY == firstY) = (((claimX, claimY), (claimID:firstIDs), (firstCount + 1)):claims)
   | True                                     = (((firstX, firstY), firstIDs, firstCount):(insertClaim1 ((claimX, claimY), claimID) claims))
+
+insertClaims :: [((Int, Int), Int)] -> [((Int, Int), [Int], Int)] -> [((Int, Int), [Int], Int)]
+insertClaims [] claims = claims
+insertClaims (claim:claims) currentClaims = insertClaim1 claim (insertClaims claims currentClaims)
 
 countOverlaps1 :: [((Int, Int), [Int], Int)] -> Int
 countOverlaps1 [] = 0
@@ -53,5 +49,4 @@ main = do
   putStrLn (show (countOverlaps1 (makeClaim1 (map extractClaim (lines fileContents)) [])))
 
   
-
 
